e048764f4488b0df43e1559f1e3f7db0c4d76284
Janaina Tannus <61756665+jmtannus@users.noreply.github.com>
2023-11-16 08:06:02 -0300

    input no scripts.js
    criando botões e funções para adicionar tarefas

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
+const button = document.querySelector('.button-add-task')
+const input = document.querySelector('.input-task')
 
+let minhaListaDeItens = []
+
+function adicionarNovaTarefa() {
+    minhaListaDeItens.push(input.value)
+
+    console.log(minhaListaDeItens)
+}
+
+function mostrarTarefas() {
+    
+}
+
+button.addEventListener('click', adicionarNovaTarefa)
